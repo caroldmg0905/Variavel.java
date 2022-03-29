c1175341446db7c7ff8bbdc8206dc1a06f72307c
caroldmg0905 <99288502+caroldmg0905@users.noreply.github.com>
2022-03-29 16:13:46 -0300

    Add files via upload

---
+++ Variavel.java
@@ -0,0 +1,12 @@
+public class Variavel {
+
+    public static void main(String[] args) {
+        int quantidade;
+        quantidade = 10;
+        System.out.println(quantidade);
+        quantidade = 15;
+        System.out.println(quantidade);
+    
+    }
+    
+}
